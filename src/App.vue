bb87374d76d4f892cdcc88d7e36c87ff8fe9362f
semanur c <semanurakoz90@gmail.com>
2022-09-30 17:49:32 +0300

    title style fixed

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,9 @@
 <template>
   <div class="container">
     <div class="box">
-      <h1 class="bg-blue-200 h-20 p-6 italic rounded-xl shadow-xl shadow-blue-300">USER LIST</h1>
-      <div class="grid grid-cols-3">
+      <h1 class="bg-blue-200 h-20 p-6 italic rounded-xl  shadow-xl shadow-blue-300">USER LIST</h1>
+      <br/>
+      <div class="grid grid-cols-3 text-blue-600/50">
         <strong>NAME</strong>
         <strong>PHONE</strong>
         <strong>E-MAIL</strong>
